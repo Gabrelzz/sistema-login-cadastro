d60e519e8c03eef5c0070086bcccc783898c889f
Gabrelzz <gabriel.ocontato22@gmail.com>
2024-01-30 17:14:54 -0300

    terceiro commit

--- formulario.php
+++ formulario.php
@@ -56,7 +56,7 @@ $conn = null;
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulário</title>
-    <link rel="stylesheet" href="css/style2.css">
+    <link rel="stylesheet" href="css/style-form.css">
 </head>
 <body>
     <div class ="box">
---
+++ home.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="css/style-home.css">
+</head>
+<body>
+    <h1>Bem vindo!</h1>
+    <div class="box">
+        <a href="login.php">Login</a>
+        <a href="formulario.php">Cadastre-se</a>
+    </div>
+</body>
+</html>
